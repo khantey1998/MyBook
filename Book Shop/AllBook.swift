fb3b5416fda76194813a9bc51fa2e6376a93108b
Student <Student@pc3-1pm.local>
2017-10-11 18:35:51 +0700

    static data

---
+++ Book Shop/AllBook.swift
@@ -0,0 +1,16 @@
+//
+//  AllBook.swift
+//  Book Shop
+//
+//  Created by Student on 10/11/17.
+//  Copyright © 2017 mybookshop. All rights reserved.
+//
+
+import Foundation
+
+struct tropeangbeysach {
+    let title: String = "Tro Peang Bey Sach"
+    let price: Int = 8000
+
+    
+}
